8c2797ccc6fc0489e484196ad2e22aafad9b083e
Jardel Pereira Zermiani <32064166+Jardelpz@users.noreply.github.com>
2020-06-23 16:39:13 -0300

    response api

---
+++ react-workshop/workshop-front/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css'
+import Header from './Header'
+import Index from './button/Index'
+import Routes from './routes'
+//const app = () => {}
+function App() {
+  return (
+    // <div className="App">
+    //   Hello World!
+    //   <Header text="Olá" value="Jardeel"/>
+    //   <Index title="Este é um títlo" cor="#F42"/>
+    //   {/* <Conteudo title="Este é um títlo" pagina={2}/> */}
+    // </div>
+    <Routes/>
+  )
+}
+
+export default App;
---
+++ react-workshop/workshop-front/src/pages/Offers/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function OfferPage(){
+    return (
+        <div>
+            <h2>Ofertas</h2>
+        </div>
+    )
+}
+
+
+export default OfferPage
